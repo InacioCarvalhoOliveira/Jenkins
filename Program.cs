ebda64e9c541e77875e64a69e23d55bfd050b0db
Inacio Oliveira <inacio.carvalho.oliveira02@gmail.com>
2023-06-23 20:45:50 -0300

    only code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 using gitConfig;
 
 namespace Jenkins
@@ -8,22 +10,43 @@ namespace Jenkins
     class Program : BranchSettings
     {
         static async Task Main(string[] args)
-        {     
+        {  
+
+            
+            // string jsonPath = "D:\\GitHub\\Jenkins\\util\\params.json";
+
+            // try
+            // {
+            //     string jsonString = File.ReadAllText(jsonPath);
+            //     BranchSettings getBranch = JsonConvert.DeserializeObject<BranchSettings>(jsonString);
+            //     Console.WriteLine($": {getBranch.BranchName}");
+            // }
+            // catch (FileNotFoundException)
+            // {
+            //     Console.WriteLine("O arquivo JSON não foi encontrado.");
+            // }
+            // catch (JsonException)
+            // {
+            //     Console.WriteLine("O arquivo JSON está em um formato inválido.");
+            // } 
             
             // Create a new process instance
             Process process = new Process(); 
-            BranchSettings config = new BranchSettings();
 
             //process.StartInfo.FileName = "x-terminal-emulator";//cinamon terminal option
             process.StartInfo.FileName = "cmd.exe";//cinamon terminal option
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.CreateNoWindow = true;
+            
             // set the privilleges of invoker as Admin and removing echoing of batch file
             string batchConfig = " @echo OFF set __COMPAT_LAYER=RunAsAdmin";
+            
             //string command = "";
+            BranchSettings config = new BranchSettings();
             process.StartInfo.Arguments = "/C " + batchConfig ;
-            config.settingProcessInfo();
+            //config.settingProcessInfo();
+            config.catchingJsonParams();
 
             // Set up event handler for reading the output asynchronously
             process.OutputDataReceived += (sender, e) =>
@@ -33,6 +56,7 @@ namespace Jenkins
             };
             // start the timewatch
             Stopwatch stopwatch = Stopwatch.StartNew();
+            
             // Start the process
             process.Start();
             process.BeginOutputReadLine();
--- gitConfig/BranchSettings.cs
+++ gitConfig/BranchSettings.cs
@@ -1,39 +1,77 @@
-using System;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace gitConfig
 {
     class BranchSettings
     {
-        private string BranchName { get; set; }          
+        public string Repository { get; set; }
+        public string Branch { get; set; }          
+        public string BranchName { get; set; }
+        public string TFSLink { get; set; }
+        
+                  
         private ProcessStartInfo gitStartInfo;
+
+        public void catchingJsonParams()
+        {
+            string jsonPath = "D:\\GitHub\\Jenkins\\util\\params.json";
+
+            try
+            {
+                // Lê o conteúdo do arquivo JSON
+                string json = File.ReadAllText(jsonPath);
+                // Faz o parsing do JSON para um objeto
+                BranchSettings obj = JsonConvert.DeserializeObject<BranchSettings>(json);
+                // Obtém o valor da propriedade BranchName
+                string branchName = obj.BranchName;
+                string branch = obj.Branch;
+                string tfsLink = obj.TFSLink;
+                string repository = obj.Repository;
+                 gitStartInfo = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                //modified Argument
+                //feature/SQPLGR-19470_MelhoriaLog_Inacio" "https://tfs.seniorsolution.com.br/Consorcio/SQCONS-WebApi/_git/SQConsorcio.API"
+                gitStartInfo.Arguments = $"clone --single-branch --branch {branch}{branchName}{tfsLink}{repository}";
+                // Start the process
+                Process gitProcess = new Process();
+                gitProcess.StartInfo = gitStartInfo;
+                gitProcess.Start();
+                // Read the output
+                //branchName = gitProcess.StandardOutput.ReadToEnd().Trim();
+
+                // Wait for the process to exit
+                gitProcess.WaitForExit();   
+                // Display the branch name
+                Console.WriteLine("Branch Repository: " + branch);
+                Console.WriteLine("Branch to be Cloned: " + branchName);
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error reading the file: " + ex.Message);
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine("Error parsing the JSON: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }       
+            
+        }
+
         public void settingProcessInfo()
         {
-            gitStartInfo = new ProcessStartInfo
-            {
-                FileName = "git",
-                Arguments = "branch --show-current",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-            //modify branch 
-            gitStartInfo.Arguments = "branch --show-current";
-            // Start the process
-            Process gitProcess = new Process();
-            gitProcess.StartInfo = gitStartInfo;
-            gitProcess.Start();
-
-            // Read the output
-            BranchName = gitProcess.StandardOutput.ReadToEnd().Trim();
-
-            // Wait for the process to exit
-            gitProcess.WaitForExit();
-
-            // Display the branch name
-            Console.WriteLine("Current branch: " + BranchName);
         
         }
     }
 
 }
+
