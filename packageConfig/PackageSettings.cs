d1199dfb7e4e53bc7c6442d091afab460c1ee842
Inacio Oliveira <inacio.carvalho.oliveira02@gmail.com>
2023-06-26 16:28:17 -0300

    novas ideias

--- packageConfig/PackageSettings.cs
+++ packageConfig/PackageSettings.cs
@@ -1,19 +1,21 @@
 using System.Collections.Generic;
+using System.Diagnostics;
+using gitConfig;
 
 namespace packageConfig
 {
-    class PackageSettings
+    class PackageSettings : BranchSettings
     {
-      public string PackageType { get; set; }// zip,default
-      public string PublishType { get; set; }// api,web,app(android/ios)
-      public string BranchSelected { get; set; }// Master,Develop,CustomBranch
-      public List<SolutionSettings> SolutionConfigs { get; set; }// 
-      public string DirToDrop { get; set; }      
-           
-      public void MyClass()
-      {
-                
-      }
+        public string? PackageType { get; set; }// zip,default
+        public string PublishType { get; set; }// api,web,app(android/ios)
+        public string BranchSelected { get; set; }// Master,Develop,CustomBranch
+        public List<SolutionSettings>? SolutionConfigs { get; set; }// 
+        public string? DirToDrop { get; set; }
+        public void buildingPakcage()
+        {           
+          
+            
+        }
     }
 
 }
---
+++ processConfig/ProcessSettings.cs
@@ -0,0 +1,7 @@
+namespace processConfig
+{
+    class ProcessSettings
+    {
+
+    }
+}
