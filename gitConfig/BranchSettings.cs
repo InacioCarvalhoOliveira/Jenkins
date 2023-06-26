7b8a6a303292c73339131fa1252d90288e5a2ddd
Inacio Oliveira <inacio.carvalho.oliveira02@gmail.com>
2023-06-26 16:27:02 -0300

    clonar branch especifica com base no json

--- gitConfig/BranchSettings.cs
+++ gitConfig/BranchSettings.cs
@@ -5,73 +5,61 @@ namespace gitConfig
 {
     class BranchSettings
     {
-        public string Repository { get; set; }
-        public string Branch { get; set; }          
+        public string Branch { get; set; }
         public string BranchName { get; set; }
         public string TFSLink { get; set; }
-        
-                  
-        private ProcessStartInfo gitStartInfo;
+        public string TagGit { get; set; }
+        public string Project { get; set; }
+        public string Repository { get; set; }
+        public string PackagePath { get; set; }
 
-        public void catchingJsonParams()
+        private ProcessStartInfo gitStartInfo;
+        public void catchingGitParams()
         {
             string jsonPath = "D:\\GitHub\\Jenkins\\util\\params.json";
-
             try
             {
                 // Lê o conteúdo do arquivo JSON
                 string json = File.ReadAllText(jsonPath);
+
                 // Faz o parsing do JSON para um objeto
-                BranchSettings obj = JsonConvert.DeserializeObject<BranchSettings>(json);
+                var obj = JsonConvert.DeserializeObject<BranchSettings>(json);
+
                 // Obtém o valor da propriedade BranchName
+                //TODO melhorar atibuição das variáveis aos objetos
                 string branchName = obj.BranchName;
                 string branch = obj.Branch;
                 string tfsLink = obj.TFSLink;
+                string tagGit = obj.TagGit;
+                string project = obj.Project;
                 string repository = obj.Repository;
-                 gitStartInfo = new ProcessStartInfo
+
+                //TODO para ser feita a clonagem é preciso um novo diretório ou um vazio                
+                string packagePath = obj.PackagePath;
+                gitStartInfo = new ProcessStartInfo
                 {
-                    FileName = "git",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
+                    FileName = "git",RedirectStandardOutput = true,
+                    UseShellExecute = false,CreateNoWindow = true
                 };
-                //modified Argument
-                //feature/SQPLGR-19470_MelhoriaLog_Inacio" "https://tfs.seniorsolution.com.br/Consorcio/SQCONS-WebApi/_git/SQConsorcio.API"
-                gitStartInfo.Arguments = $"clone --single-branch --branch {branch}{branchName}{tfsLink}{repository}";
-                // Start the process
+                gitStartInfo.Arguments = $"clone --single-branch --branch {branch}/{branchName}{tfsLink}/{project}/{tagGit}/{repository}/ {packagePath}";
+                
                 Process gitProcess = new Process();
                 gitProcess.StartInfo = gitStartInfo;
                 gitProcess.Start();
-                // Read the output
-                //branchName = gitProcess.StandardOutput.ReadToEnd().Trim();
+                gitProcess.WaitForExit();
 
-                // Wait for the process to exit
-                gitProcess.WaitForExit();   
-                // Display the branch name
                 Console.WriteLine("Branch Repository: " + branch);
                 Console.WriteLine("Branch to be Cloned: " + branchName);
-
             }
             catch (IOException ex)
-            {
-                Console.WriteLine("Error reading the file: " + ex.Message);
-            }
-            catch (JsonException ex)
-            {
-                Console.WriteLine("Error parsing the JSON: " + ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }       
-            
-        }
+            { Console.WriteLine("Error reading the file: " + ex.Message); }
 
-        public void settingProcessInfo()
-        {
-        
+            catch (JsonException ex)
+            { Console.WriteLine("Error parsing the JSON: " + ex.Message); }
+
+            catch (Exception ex)
+            { Console.WriteLine("Error: " + ex.Message); }
         }
     }
-
 }
 
